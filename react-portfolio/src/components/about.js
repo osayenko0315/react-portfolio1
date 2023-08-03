41307f8c84350b92511f2504aaad263df88d575e
osayenko0315 <126353984+osayenko0315@users.noreply.github.com>
2023-08-02 19:58:37 -0500

    changes

---
+++ react-portfolio/src/components/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Oleg Sayenko</h2>
+            <hr></hr>
+            <div>
+                
+            </div> </section>
+    )
+}
+
+export default About;
